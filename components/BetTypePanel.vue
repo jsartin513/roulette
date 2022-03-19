07a299604abc3b7bab5ae1026545d3e6af6b88f1
jdoherty513 <jdoherty513@gmail.com>
2022-03-19 18:52:32 -0400

    Move odds pane into its own componnennt

--- components/BetTypePanel.vue
+++ components/BetTypePanel.vue
@@ -13,26 +13,15 @@
         {{ betType.name }}
       </div>
     </div>
-    <div>
-      <h3 class="text-md text-center border-t border-b m-2">Odds</h3>
-      <div class="odds">
-        <span class="label">Payout: </span>{{ currenntPotentialPayout }}
-      </div>
-      <div class="odds">
-        <span class="label">Likelihood: </span>{{ currentLikelihood }}
-      </div>
-      <div class="odds">
-        <span class="label">Win requirements: </span
-        >{{ currentWinRequiremennts }}
-      </div>
-      <p class="odds">How does this work? Lorem ipsum and whatnot.</p>
-    </div>
+    <oddsPane v-if="canComputePayout" :selected-bet-type="selectedBetType" />
   </div>
 </template>
 
 <script>
+import OddsPane from './OddsPane'
 export default {
   name: 'BetTypePanel',
+  components: { oddsPane: OddsPane },
   props: {
     betTypes: { type: Array, default: null },
     selectedBetType: { type: Object, default: null },
@@ -50,31 +39,7 @@ export default {
       return allBets
     },
     canComputePayout() {
-      return this.selectedBetType && this.selectedBetType.spaces.length > 0;
-    },
-    dollarsBet() {
-      return this.selectedBetType.spaces.length;
-    },
-    oddsOnOneSquare(){
-      return 35;
-    },
-    currenntPotentialPayout() {
-      if (this.canComputePayout) {
-        return `${this.oddsOnOneSquare}:1`
-      }
-      return ''
-    },
-    currentLikelihood() {
-      if (this.canComputePayout) {
-        return `${this.dollarsBet.toString()} / 38`;
-      }
-      return ''
-    },
-    currentWinRequiremennts() {
-      if (this.canComputePayout) {
-        return `Win more than one out of every ${Math.round(this.oddsOnOneSquare / this.dollarsBet)} bets`
-      }
-      return ''
+      return this.selectedBetType && this.selectedBetType.spaces.length > 0
     },
   },
   methods: {
---
+++ components/OddsPane.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3 class="text-md text-center border-t border-b m-2">Odds</h3>
+    <div class="odds">
+      <span class="label">Payout: </span>{{ currenntPotentialPayout }}
+    </div>
+    <div class="odds">
+      <span class="label">Likelihood: </span>{{ currentLikelihood }}
+    </div>
+    <div class="odds">
+      <span class="label">Win requirements: </span>{{ currentWinRequiremennts }}
+    </div>
+    <p class="odds">How does this work? Lorem ipsum and whatnot.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OddsPane',
+  props: { selectedBetType: {type: Object, default: null}},
+  computed: {
+    dollarsBet() {
+      return this.selectedBetType.spaces.length
+    },
+    oddsOnOneSquare() {
+      return 35
+    },
+    currenntPotentialPayout() {
+      return `${this.oddsOnOneSquare}:1`
+    },
+    currentLikelihood() {
+      return `${this.dollarsBet.toString()} / 38`
+    },
+    currentWinRequiremennts() {
+      return `Win more than one out of every ${Math.round(
+        this.oddsOnOneSquare / this.dollarsBet
+      )} bets`
+    },
+  },
+}
+</script>
