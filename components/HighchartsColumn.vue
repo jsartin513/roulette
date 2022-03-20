c1ad9e4bd6ee48cab89d1bc148dc856371f13cb5
jdoherty513 <jdoherty513@gmail.com>
2022-03-20 19:52:09 -0400

    hardcode color

--- components/HighchartsColumn.vue
+++ components/HighchartsColumn.vue
@@ -26,21 +26,27 @@ export default {
     selectedSpaces: { type: Array, default: null },
   },
   computed: {
-    chartOptions() {
+    seriesData() {
       const seriesData = this.slices.map((slice) => {
         const numberAsString = this.convertSliceNumber(slice.number)
+        const anySelected = this.selectedSpaces && this.selectedSpaces.length > 0
         const isSelected = this.selectedSpaces
           ? this.selectedSpaces.findIndex(
               (selectedSpace) => selectedSpace === numberAsString
             ) !== -1
           : false
+        const showOpaqueColor = anySelected && !isSelected
+        const convertedColor = this.convertColor(slice.color, showOpaqueColor)
         return {
           name: numberAsString,
           y: 1,
-          color: slice.color,
+          color: convertedColor,
           selected: isSelected,
         }
       })
+      return seriesData
+    },
+    chartOptions() {
       return {
         chart: {
           height: 600,
@@ -54,17 +60,31 @@ export default {
         title: null,
         plotOptions: {
           pie: {
-            allowPointSelect: true,
+            allowPointSelect: false,
             dataLabels: {
               enabled: true,
               format: '{point.name}',
               style: { color: '#FFFFFF' },
               distance: -10,
             },
+            point: {opacity: (this.selectedSpaces && this.selectedSpaces.length > 0) ? 0.2 : 0.2}
+          },
+          series: {
+            // opacity: (this.selectedSpaces && this.selectedSpaces.length > 0) ? 0.2 : 1,
+            // opacity: (this.selectedSpaces && this.selectedSpaces.length > 0) ? 0.2 : 1,
+            states: { select: {opacity: 1, normal: {opacity: 0.1}}}
           },
         },
-        series: [{ name: 'numbers', colorByPoint: true, data: seriesData }],
+        series: [
+          { name: 'numbers', colorByPoint: true, data: this.seriesData },
+        ],
       }
+    },
+  },
+  watch: {
+    selectedSpaces: {
+      handler(newValue, oldValue) {},
+      deep: true,
     },
   },
   methods: {
@@ -74,6 +94,70 @@ export default {
       }
       return number.toString()
     },
+    convertColor(color, showOpaqueColor) {
+      if (!showOpaqueColor){
+        return color;
+      }
+      if (color==="black"){
+        return 'rgba(0,0,0,0.2)';
+      }
+      else if (color==="red"){
+        return 'rgba(255,0,0,0.2)';
+      }
+      else {
+        return 'rgba(0,128,0,0.2)';
+      }
+
+    }
   },
 }
 </script>
+
+<style scoped>
+.highcharts-figure,
+.highcharts-data-table table {
+  min-width: 420px;
+  max-width: 800px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #ebebeb;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+}
+
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+
+.highcharts-data-table th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+
+.highcharts-data-table td,
+.highcharts-data-table th,
+.highcharts-data-table caption {
+  padding: 0.5em;
+}
+
+.highcharts-data-table thead tr,
+.highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
+
+path.inactive {
+  opacity: .1 !important;
+}
+</style>
