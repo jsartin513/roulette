f8f18f6cfd754210c00d61786422e5cdbc7d602b
jdoherty513 <jdoherty513@gmail.com>
2022-03-26 22:21:09 -0400

    fix by adding nnew empty states

---
+++ components/NoSelectedBetPane.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3 class="text-md text-center border-t border-b m-2">Bet info</h3>
+    <p>Please select a bet to see odds and explanation.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoSelectedBetPane'
+}
+</script>
